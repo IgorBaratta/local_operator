d664cf353fe9990e3176890185c9c7675af18be4
IgorBaratta <igorbaratta@gmail.com>
2021-05-05 15:34:07 +0100

    fix xtensor issue

--- main.cpp
+++ main.cpp
@@ -1,8 +1,5 @@
-// #define XTENSOR_USE_XSIMD 1
 #include "problem.h"
 #include <iostream>
-#include <xtensor/xtensor.hpp>
-#include <xtensor/xio.hpp>
 #include <ufc.h>
 #include <chrono>
 
@@ -14,14 +11,15 @@ int main(int argc, char *argv[])
     int ndofs_cell = a.finite_elements[1]->space_dimension;
     auto &&kernel = a.integrals(ufc_integral_type::cell)[0]->tabulate_tensor;
 
-    xt::xtensor<double, 2> coordinate_dofs = {{0.1, 0.0, 0.1}, {1.0, 0.0, 0.1}, {0.0, 1.0, 0.0}, {0.0, 0.0, 1.0}};
+    const double coordinate_dofs[12] = {0.1, 0.0, 0.1, 1.0, 0.0, 0.1, 0.0, 1.0,
+                                        0.0, 0.0, 0.0, 1.0};
 
     double Ae[ndofs_cell * ndofs_cell];
 
     auto start = std::chrono::steady_clock::now();
     for (int c = 0; c < ncells; c++)
     {
-        kernel(Ae, nullptr, nullptr, coordinate_dofs.data(), 0, 0, 0);
+        kernel(Ae, nullptr, nullptr, coordinate_dofs, 0, 0, 0);
     }
     auto end = std::chrono::steady_clock::now();
     double duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1.e6;
