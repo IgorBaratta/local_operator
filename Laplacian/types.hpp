f46b9f7005f8ad3700db81ab4658c76a270b2fa7
Chris Richardson <chris@bpi.cam.ac.uk>
2023-04-03 15:58:58 +0000

    Add some a64fx results

--- Laplacian/types.hpp
+++ Laplacian/types.hpp
@@ -4,9 +4,11 @@
 
 namespace stdex = std::experimental;
 
+
 template <std::size_t Precision, std::size_t BatchSize>
 struct VectorExtensions
 {
+typedef double doubleB __attribute__((vector_size(BatchSize * sizeof(double))));
     using S = std::conditional_t<Precision == 8, double, float>;
-    using T = std::conditional_t<BatchSize == 1, S, stdex::fixed_size_simd<S, BatchSize>>;
+    using T = std::conditional_t<BatchSize == 1, S, doubleB>;
 };
