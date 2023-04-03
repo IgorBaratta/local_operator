1774e9097c1849e964d56255426dc7d233ac4c5c
Chris Richardson <chris@bpi.cam.ac.uk>
2023-04-03 16:43:27 +0000

    Add more results

--- MassHex/types.hpp
+++ MassHex/types.hpp
@@ -1,20 +1,14 @@
-#include <cstddef>     // for std::size_t
-#include <type_traits> // for std::conditional_t
+#include <cstddef>              // for std::size_t
+#include <type_traits>          // for std::conditional_t
+#include <experimental/simd>    // for std::experimental
+
+namespace stdex = std::experimental;
+
 
 template <std::size_t Precision, std::size_t BatchSize>
 struct VectorExtensions
 {
-    #if PRECISION == 8
-        typedef double T __attribute__((vector_size(BatchSize * sizeof(double))));
-    #else
-        typedef float T __attribute__((vector_size(BatchSize * sizeof(float))));
-    #endif
+typedef double doubleB __attribute__((vector_size(BatchSize * sizeof(double))));
     using S = std::conditional_t<Precision == 8, double, float>;
-};
-
-template <std::size_t Precision>
-struct VectorExtensions<Precision, 1>
-{
-    using T = std::conditional_t<Precision == 8, double, float>;
-    using S = std::conditional_t<Precision == 8, double, float>;
+    using T = std::conditional_t<BatchSize == 1, S, doubleB>;
 };
