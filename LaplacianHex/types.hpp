01cd966f059a29c4332295ebfeae010dbc2a8a64
Chris Richardson <chris@bpi.cam.ac.uk>
2023-04-06 12:22:54 +0000

    update types.h

--- LaplacianHex/types.hpp
+++ LaplacianHex/types.hpp
@@ -8,7 +8,7 @@ namespace stdex = std::experimental;
 template <std::size_t Precision, std::size_t BatchSize>
 struct VectorExtensions
 {
-typedef double doubleB __attribute__((vector_size(BatchSize * sizeof(double))));
     using S = std::conditional_t<Precision == 8, double, float>;
-    using T = std::conditional_t<BatchSize == 1, S, doubleB>;
+    typedef S scalarB __attribute__((vector_size(BatchSize * sizeof(S))));
+    using T = std::conditional_t<BatchSize == 1, S, scalarB>;
 };
