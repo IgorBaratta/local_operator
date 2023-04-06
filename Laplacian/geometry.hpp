ecc31ca20ac5469ca184fddced89e051a3e34118
IgorBaratta <igorbaratta@gmail.com>
2023-04-06 09:33:21 +0100

    update laplacian geometry

--- Laplacian/geometry.hpp
+++ Laplacian/geometry.hpp
@@ -5,8 +5,17 @@
 template <typename T, typename S>
 std::vector<T> create_geometry(int num_batches, int batch_size, int geom_size)
 {
-    std::array<double, 24> coords = {0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0,
-                                     1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0};
+
+    // Geometry of the cell
+    //  [[0. 0. 0.]
+    //  [1. 0. 0.]
+    //  [1. 1. 0.]
+    //  [1. 1. 1.]]
+    // Coords stores in column major format XXXXYYYYZZZZ
+    std::array<double, 12> coords = {0., 1., 1., 1.,
+                                     0., 0., 1., 1.,
+                                     0., 0., 0., 1.};
+
     std::vector<T> geometry(num_batches * geom_size);
     if constexpr (std::is_same<T, double>::value or std::is_same<T, float>::value)
     {
