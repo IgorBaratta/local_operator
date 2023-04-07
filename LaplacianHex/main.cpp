b2bfeab330fdb769f482a06ecebcf5743cc460bc
IgorBaratta <igorbaratta@gmail.com>
2023-04-07 19:59:05 +0100

    update laplacian hex

--- LaplacianHex/main.cpp
+++ LaplacianHex/main.cpp
@@ -11,27 +11,17 @@
 #include <cassert>
 #include <any>
 
-#ifndef PRECISION
-#error Floating point precision not defined.
-#endif
-
-#ifndef BATCH_SIZE
-#error Batch size not defined.
-#endif
-
-#ifndef DEGREE
-#error Polynomial degree not defined.
-#endif
-
-#ifndef BLOCK_SIZE
-#error Block size not defined.
-#endif
-
 int main(int argc, char *argv[])
 {
 
-  using T = VectorExtensions<PRECISION, BATCH_SIZE>::T;
-  using S = VectorExtensions<PRECISION, BATCH_SIZE>::S;
+  constexpr int precision = PRECISION;
+  constexpr int batch_size = BATCH_SIZE;
+  constexpr int bs = BLOCK_SIZE;
+  constexpr int P = DEGREE;
+  constexpr bool precompute = PRECOMPUTE;
+
+  using T = VectorExtensions<precision, batch_size>::T;
+  using S = VectorExtensions<precision, batch_size>::S;
   constexpr int global_size = 10000000;
 
   MPI_Init(&argc, &argv);
@@ -41,17 +31,15 @@ int main(int argc, char *argv[])
     int mpi_rank;
     MPI_Comm_rank(comm, &mpi_rank);
 
-    constexpr int bs = BLOCK_SIZE;
-    constexpr int P = DEGREE;
-
-    Operator<T, S, P, bs> op;
+    Operator<T, S, P, bs, precompute> op;
 
     // Const data from kernel
+    constexpr int Nq = (P + 3) * (P + 3) * (P + 3);
     constexpr int local_size = op.num_dofs;
     constexpr int stride = op.num_dofs + 4;
     constexpr int num_cells = global_size / op.num_dofs;
     constexpr int num_batches = num_cells / BATCH_SIZE;
-    constexpr int geom_size = 8 * 3;
+    constexpr int geom_size = precompute ? Nq * 6 : 8 * 3;
 
     // Allocate and initialize data
     std::vector<T> A(num_batches * local_size);
@@ -60,12 +48,20 @@ int main(int argc, char *argv[])
     T one = {1};
     T zero = {0};
     T reference = {0};
-
-    // Create geometry and coefficients
-    std::vector<T> geometry = create_geometry<T, S>(num_batches, BATCH_SIZE, geom_size);
-    std::vector<T> coefficients(num_batches * stride);
     auto set_ = [one](auto &e)
     { e = one; };
+
+    // Create geometry and coefficients
+    std::vector<T> geometry(geom_size * num_batches);
+    if (precompute)
+    {
+      std::for_each(geometry.begin(), geometry.end(), set_);
+    }
+    else
+    {
+      geometry = create_geometry<T, S>(num_batches, BATCH_SIZE, geom_size);
+    }
+    std::vector<T> coefficients(num_batches * stride);
     std::for_each(coefficients.begin(), coefficients.end(), set_);
 
     // Sanity check: Are we computing the correct values?
@@ -105,7 +101,9 @@ int main(int argc, char *argv[])
 
     if (mpi_rank == 0)
     {
-      std::cout << PRECISION << ", " << BATCH_SIZE << ", " << num_cells << ", " << DEGREE << ", " << max_time;
+      std::cout << precision << ", " << batch_size << ", ";
+      std::cout << num_cells << ", " << P << ", " << max_time << ", ";
+      std::cout << bs << ", " << precompute;
     }
   }
   MPI_Finalize();
--- LaplacianHex/run.sh
+++ LaplacianHex/run.sh
@@ -2,15 +2,17 @@
 
 export CXX=g++
 export CXX_FLAGS="-march=native -Ofast"
-export BATCH_SIZE=1
+
+export BATCH_SIZE=8
+export BLOCK_SIZE=8
 export PRECISION=8
 export NUM_PROCS=1
+export PRECOMPUTE=1
 
-
-for DEGREE in 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+for DEGREE in 1
 do
     rm -rf build
-    cmake -B build/ -DCMAKE_CXX_FLAGS="${CXX_FLAGS}" -DPRECISION=${PRECISION} -DBATCH_SIZE=${BATCH_SIZE} -DDEGREE=${DEGREE} .
+    cmake -B build/ -DCMAKE_CXX_FLAGS="${CXX_FLAGS}" -DPRECISION=${PRECISION} -DBATCH_SIZE=${BATCH_SIZE} -DDEGREE=${DEGREE} -DBLOCK_SIZE=${BLOCK_SIZE} -DPRECOMPUTE=${PRECOMPUTE} .
     cmake --build build -j 10
     mpirun -n ${NUM_PROCS} ./build/benchmark >> out.txt
     echo ", '${CXX_FLAGS}'" >> out.txt
