b75659cf45698eade8132558de6dcb830878fefb
Chris Richardson <chris@bpi.cam.ac.uk>
2023-05-16 11:18:21 +0000

    Some data for a64fx

--- Mass/run-a64fx-gcc.sh
+++ Mass/run-a64fx-gcc.sh
@@ -1,7 +1,8 @@
 #bin/bash
 
-export CXX=/rds/user/ia397/hpc-work/spack/opt/spack/linux-rocky8-zen/gcc-8.4.0/gcc-12.2.0-vabb3h6ueyk5rgnkslaixjzbuoacjjxo/bin/g++
-export CC=/rds/user/ia397/hpc-work/spack/opt/spack/linux-rocky8-zen/gcc-8.4.0/gcc-12.2.0-vabb3h6ueyk5rgnkslaixjzbuoacjjxo/bin/gcc
+export CXX=/snx11273/home/ri-crichardson/spack/opt/spack/linux-rhel8-a64fx/gcc-10.3.0/gcc-12.2.0-zf65tfzmboe3t6lkegf4rrw2zwnprvtn/bin/g++
+export CC=/snx11273/home/ri-crichardson/spack/opt/spack/linux-rhel8-a64fx/gcc-10.3.0/gcc-12.2.0-zf65tfzmboe3t6lkegf4rrw2zwnprvtn/bin/gcc
+
 
 export NUM_PROCS=48
 
@@ -11,7 +12,7 @@ run () {
         do
             rm -rf build
             cmake -B build/ -DCMAKE_CXX_FLAGS="${CXX_FLAGS}" -DPRECISION=$1 -DBATCH_SIZE=$2 -DDEGREE=${DEGREE} .
-            cmake --build build -j 10
+            cmake --build build 
         for i in {1..3}
         do
             mpirun -n ${NUM_PROCS} ./build/benchmark >> $3
