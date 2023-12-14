3249b780d1fa2cdf3ded362bf776a750920cbe40
Michal Habera <michal.habera@rafinex.com>
2023-12-14 10:22:00 +0000

    LLVM17 - results now match GCC13.

--- run.sh
+++ run.sh
@@ -6,7 +6,9 @@
 #SBATCH --output=mpi_test_%j.log     # Path to the standard output and error files relative to the working directory
 #SBATCH -p small
 
-spack env activate ffcx
 spack load cmake
+spack load gcc@13.2.0+binutils
+spack load llvm
+spack env activate ffcx
 python3 run.py --problem Elasticity  --degree 2 --form_compiler=ffcx --action --global_size 1000000 --output_file=output/elasticity_2_action.csv
 python3 run.py --problem Elasticity  --degree 6 --form_compiler=ffcx --action --global_size 10000000 --output_file=output/elasticity_6_action.csv
